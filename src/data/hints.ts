4771de6e6a917acd8e04fbdba89e1be9035cad7d
Thauã Magalhães <thauanlucascpl@gmail.com>
2024-10-26 00:14:59 -0300

    feat: initial hiints list added

--- src/data/hints.ts
+++ src/data/hints.ts
@@ -1,13 +1,51 @@
 import { Routes } from "@/routes/routes";
 
 export type Hint = {
-  key: string;
+  key: HintsEnum;
   route: string;
+  hint: string;
 };
+
+export enum HintsEnum {
+  agorasodeus = "agorasodeus",
+  caminhodastrevas = "caminhodastrevas",
+  eisqueletal = "eisqueletal",
+  luzdanoite = "luzdanoite",
+  magiacruel = "magiacruel",
+  sgmk = "sgmk",
+}
 
 export const hints: Hint[] = [
   {
-    key: "sgmk",
-    route: Routes.hint("sgmk"),
+    key: HintsEnum.agorasodeus,
+    route: Routes.hint(HintsEnum.agorasodeus),
+    hint: "Agora so deus",
+  },
+  {
+    key: HintsEnum.caminhodastrevas,
+    route: Routes.hint(HintsEnum.caminhodastrevas),
+    hint: "Caminho das trevas",
+  },
+  {
+    key: HintsEnum.eisqueletal,
+    route: Routes.hint(HintsEnum.eisqueletal),
+    hint: "Ei esqueletal",
+  },
+  {
+    key: HintsEnum.luzdanoite,
+    route: Routes.hint(HintsEnum.luzdanoite),
+    hint: "Luz da noite",
+  },
+  {
+    key: HintsEnum.magiacruel,
+    route: Routes.hint(HintsEnum.magiacruel),
+    hint: "Magia cruel",
+  },
+  {
+    key: HintsEnum.sgmk,
+    route: Routes.hint(HintsEnum.sgmk),
+    hint: "Sgmk",
   },
 ];
+
+export const getHint = (key: string) => hints.find((hint) => hint.key === key);
